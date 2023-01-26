afd167e6d3be5c383e63ecd96ad46e0c244cb4a4
root <root@T3-6660174.localdomain>
2023-01-26 10:29:38 -0700

    update

--- modules/system-config/scripts/install_docker.sh
+++ modules/system-config/scripts/install_docker.sh
@@ -10,3 +10,19 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 ## install docker
 apt-get update && apt-get install docker-ce docker-ce-cli containerd.io -y 
+
+## now setup telegraf on the local machine 
+wget -qO- https://repos.influxdata.com/influxdb.key | apt-key add -
+
+## install telegraf on local system
+apt-get install telegraf
+
+
+## copy new config file into place
+cp ./install_tig/telegraf/telegraf.conf /etc/telegraf/telegraf.conf
+
+## start running
+systemctl daemon-reload
+systemctl enable telegraf
+systemctl start telegraf
+systemctl restart telegraf
